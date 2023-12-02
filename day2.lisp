186324dca8a71626363896324972ca6718ab5dc2
Blake Watkins <blakewatkins@gmail.com>
2023-12-03 11:28:40 +1300

    Clean up day 2.

--- day2.lisp
+++ day2.lisp
@@ -1,14 +1,11 @@
 (in-package :aoc-2023)
 
-(defun parse-color ()
-  (parse-keyword #'alphanumericp))
-
-(defun parse-cube-set ()
+(defun parse-roll ()
   (parse-list
    (with-monad
      (assign amount (parse-number))
      (parse-string " ")
-     (assign color (parse-color))
+     (assign color (parse-keyword #'alphanumericp))
      (unit (list amount color)))
    ", "))
 
@@ -17,41 +14,38 @@
     (parse-string "Game ")
     (assign id (parse-number))
     (parse-string ": ")
-    (assign sets (parse-list (parse-cube-set) "; "))
-    (unit (list id sets))))
+    (assign rolls (parse-list (parse-roll) "; "))
+    (unit (list id rolls))))
 
-(defun test-game (cubes game)
+(defun game-valid? (limits game)
   (iter
-    (for (amount color) in cubes)
-    (always (iter
-              (for roll-set in (second game))
-              (always (iter
-                        (for roll in roll-set)
-                        (never (and (eq (second roll) color)
-                                    (> (first roll) amount)))))))))
+    (for (limit-amount limit-color) in limits)
+    (always
+     (iter
+       (for rolls in (second game))
+       (always
+        (iter
+          (for (roll-amount roll-color) in rolls)
+          (never (and (eq roll-color limit-color)
+                      (> roll-amount limit-amount)))))))))
 
-(defun day2 (input)
-  (let ((games (run-parser (parse-lines (parse-game)) input)))
-    (iter
-      (for game in games)
-      (when (test-game '((12 :red) (13 :green) (14 :blue)) game)
-        (sum (first game))))))
+(defun game-power (game)
+  (let ((maxs (iter
+                (with maximums = (make-hash-table :test 'eq))
+                (for roll-set in (second game))
+                (iter
+                  (for (amount color) in roll-set)
+                  (setf (gethash color maximums)
+                        (max amount (gethash color maximums 0))))
+                (finally (return maximums)))))
+    (reduce #'* (mapcar (lambda (c) (gethash c maxs 0)) '(:red :blue :green)))))
 
-(defun game-power (cubes)
-  (reduce #'*
-          (mapcar (lambda (color) (gethash color cubes 0)) '(:red :blue :green))))
+(defun day2 (input &key (part 1))
+  (let ((limits '((12 :red) (13 :green) (14 :blue)))
+        (games (run-parser (parse-lines (parse-game)) input)))
+    (if (= part 1)
+        (iter
+          (for game in games)
+          (when (game-valid? limits game) (sum (first game))))
+        (reduce #'+ (mapcar #'game-power games)))))
 
-(defun day2-2 (input)
-  (let ((games (run-parser (parse-lines (parse-game)) input)))
-    (reduce #'+
-            (iter
-              (for game in games)
-              (collect
-                  (iter
-                    (with maximums = (make-hash-table :test 'eq))
-                    (for roll-set in (second game))
-                    (iter
-                      (for (amount color) in roll-set)
-                      (setf (gethash color maximums)
-                            (max amount (gethash color maximums 0))))
-                    (finally (return (game-power maximums)))))))))
