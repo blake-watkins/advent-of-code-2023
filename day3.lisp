accddb67a466eb3bde673625d4693496a050addc
Blake Watkins <blakewatkins@gmail.com>
2023-12-04 09:32:34 +1300

    Tidy up day 3.

--- day3.lisp
+++ day3.lisp
@@ -11,15 +11,26 @@
 
 (defun parse-line ()
   (with-monad
-    (assign ret
-	    (one-or-more
-	     (either (parse-indexed-number)
-		     (then (parse-rc-character ".") (unit '()))
-		     (with-monad
-		       (assign sym (parse-rc-character (complement #'whitespace-char-p)))
-		       (unit (cons :symbol sym))))))
+    (assign ret (one-or-more
+		 (either (parse-indexed-number)
+			 (then (parse-rc-character ".") (unit '()))
+			 (with-monad
+			   (assign sym (parse-rc-character
+					(complement #'whitespace-char-p)))
+			   (unit (cons :symbol sym))))))
     (parse-rc-newline)
     (unit ret)))
+
+(defun get-numbers-and-symbols (input)
+  (let ((parsed (run-rc-parser (one-or-more (parse-line)) input)))
+    (iter outer
+      (for line in parsed)
+      (iter
+	(for (type . item) in line)
+	(case type
+	  (:number (in outer (collect item into numbers)))
+	  (:symbol (in outer (collect item into symbols)))))
+      (finally (return-from outer (list numbers symbols))))))
 
 (defun number-near-symbol? (number symbol)
   (destructuring-bind (((sr sc) (er ec)) val) number
@@ -29,36 +40,30 @@
       (and (<= (1- sc) c ec) (<= (1- sr) r (1+ sr))))))
 
 (defun numbers-near-symbol (numbers symbol)
+  "Return list of all numbers near to SYMBOL."
   (iter
     (for number in numbers)
     (when (number-near-symbol? number symbol)
       (collect number))))
 
 (defun gear-ratio (numbers symbol)
+  "Return gear ratio if SYMBOL is a gear, NIL otherwise."
   (when (char= #\* (second symbol))
     (let ((nearby-numbers (numbers-near-symbol numbers symbol)))
       (when (= 2 (length nearby-numbers))
 	(reduce #'* nearby-numbers :key #'second)))))
 
+
 (defun day3 (input &key (part 1))
-  (let ((parsed (run-rc-parser (one-or-more (parse-line)) input)))
-    (destructuring-bind (numbers symbols)
+  (destructuring-bind (numbers symbols) (get-numbers-and-symbols input)
+    (if (= part 1)
 	(iter outer
-	  (for line in parsed)
-	  (iter
-	    (for (type . item) in line)
-	    (case type
-	      (:number (in outer (collect item into numbers)))
-	      (:symbol (in outer (collect item into symbols)))))
-	  (finally (return-from outer (list numbers symbols))))
-      (if (= part 1)
-	  (iter outer
-	    (for number in numbers)
-	    (iter
-	      (for symbol in symbols)
-	      (when (number-near-symbol? number symbol)
-		(in outer (sum (second number))))))
+	  (for number in numbers)
 	  (iter
 	    (for symbol in symbols)
-	    (for gear-ratio = (gear-ratio numbers symbol))
-	    (when gear-ratio (sum gear-ratio)))))))
+	    (when (number-near-symbol? number symbol)
+	      (in outer (sum (second number))))))
+	(iter
+	  (for symbol in symbols)
+	  (for gear-ratio = (gear-ratio numbers symbol))
+	  (when gear-ratio (sum gear-ratio))))))
