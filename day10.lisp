d0c0f0de2362cf6dd5cbef8b9d00afa0ea86d35a
Blake Watkins <blakewatkins@gmail.com>
2023-12-11 15:51:41 +1300

    Cleaned up day 10.

--- day10.lisp
+++ day10.lisp
@@ -32,21 +32,22 @@
 			     (#\F (:south :east))
 			     (#\S (:north :south :east :west))))
 
-(defun connected? (a b map)
-  (let* ((offset (point- b a))
-	 (connections
-	   (second (find (gethash a map) *pipe-types* :test 'eq :key #'first)))
-	 (dir (second (find offset *directions* :test 'equal :key #'first))))
-    (and dir (member dir connections))))
+(defun pipe-connected? (a b map)
+  (flet ((connected-one-way? (a b map)
+	   (let* ((offset (point- b a))
+		  (dir (find offset *directions* :test 'equal :key #'first))
+		  (dir-name (second dir))
+		  (map-char (gethash a map))
+		  (pipe-type (find map-char *pipe-types* :test 'eq :key #'first))
+		  (connection-names (second pipe-type)))
+	     (and dir-name (member dir-name connection-names)))))
+    (and (connected-one-way? a b map) (connected-one-way? b a map))))
 
-(defun both-connected? (a b map)
-  (and (connected? a b map) (connected? b a map)))
-
-(defun next-pos (pos map)
+(defun pipe-neighbours (pos map)
   (iter
-    (for (offset nil) in *directions*)
+    (for (offset dir) in *directions*)
     (for next-pos = (point+ offset pos))
-    (when (both-connected? pos next-pos map) (collect next-pos))))
+    (when (pipe-connected? pos next-pos map) (collect (list next-pos dir)))))
 
 (defun mark-loop (loop-squares)
   (iter
@@ -55,60 +56,58 @@
     (setf (gethash square ret) :loop)
     (finally (return ret))))
 
+(define-condition outside-map (condition) ())
+
+(defun can-mark? (pos marked map)
+  (when (not (gethash pos map)) (signal 'outside-map))
+  (and (gethash pos map) (not (gethash pos marked))))
+
+(defun unmarked-neighbours (pos marked map)
+  (iter
+    (for (offset nil) in *directions*)
+    (for next-square = (point+ pos offset))
+    (when (can-mark? next-square marked map) (collect next-square))))
+
 (defun mark-connected (from mark marked map)
-  (if (or (gethash from marked) (not (gethash from map)))
-      marked
-      (iter
-	(for vertex-info
-	     in-bfs-from from
-	     neighbours (lambda (from)
-			  (iter
-			    (for (offset nil) in *directions*)
-			    (for next-square = (point+ from offset))
-			    (when (and (gethash next-square map)
-				       (not (gethash next-square marked)))
-			      (collect next-square))))
-	     test 'equal
-	     single t)
-	(setf (gethash (first vertex-info) marked) mark)
-	(finally (return marked)))))
+  (when (can-mark? from marked map)
+    (iter
+      (for (pos . nil)
+	   in-bfs-from from
+	   neighbours (lambda (from) (unmarked-neighbours from marked map))
+	   test 'equal
+	   single t)
+      (setf (gethash pos marked) mark))))
 
-(defun turn (pos dir ccw)
-  (destructuring-bind (offset offset-dir)
-      (elt *directions* (mod (+ (position dir *directions* :key #'second)
-				(ecase ccw (:cw 1) (:ccw -1)))
-			     (length *directions*)))
-    (list (point+ pos offset) offset-dir)))
+(defun turn (dir rotation)
+  (elt *directions* (mod (+ (position dir *directions* :key #'second)
+			    (ecase rotation (:cw 1) (:ccw -1)))
+			 (length *directions*))))
 
-(defun mark-connected-from-direction (pos dir marked map)
-  (mark-connected (first (turn pos dir :cw)) :a marked map)
-  (mark-connected (first (turn pos dir :ccw)) :b marked map))
+(defun mark-loop-sides (pos dir marked map)
+  (iter
+    (with outside-mark = nil)
+    (for (rotation mark) in '((:cw :a) (:ccw :b)))
+    (handler-case (mark-connected (point+ pos (first (turn dir rotation)))
+				  mark marked map)
+      (outside-map () (setf outside-mark mark)))
+    (finally (return outside-mark))))
 
 (defun walk-loop (start-pos marked map)
   (iter
+    (with outside-mark = nil)
     (for prev-pos previous pos)
     (for pos initially start-pos then next-pos)
-    (for next-pos = (first (remove prev-pos (next-pos pos map) :test 'equal)))
-    (for dir =
-	 (second (find (point- next-pos pos) *directions* :test 'equal :key #'first)))
+    (for neighbours =
+	 (remove prev-pos (pipe-neighbours pos map) :test 'equal :key #'first))
+    (for (next-pos dir) = (first neighbours))
     (for prev-dir previous dir)
+    (setf outside-mark
+	  (or (mark-loop-sides pos dir marked map) outside-mark))
     (when (and prev-dir (not (eq prev-dir dir)))
-      (mark-connected-from-direction pos prev-dir marked map))
-    (mark-connected-from-direction pos dir marked map)
+      (setf outside-mark
+	    (or (mark-loop-sides pos prev-dir marked map) outside-mark)))
     (until (and prev-pos (equal pos start-pos)))
-    (finally (return marked))))
-
-(defun day10 (input)
-  (destructuring-bind (start-pos map) (get-map input)
-    (iter     
-      (for (pos from steps root)
-	   in-bfs-from start-pos
-	   neighbours (lambda (n) (next-pos n map))
-	   test 'equal
-	   single t)
-      (collect pos into loop)
-      (maximizing steps)
-      (finally (return (list steps (hash-table-count map) (count-table (walk-loop start-pos (mark-loop loop) map) )))))))
+    (finally (return outside-mark))))
 
 (defun count-table (marked)
   (iter
@@ -116,3 +115,24 @@
     (for (key val) in-hashtable marked)
     (incf (gethash val ret 0))
     (finally (return ret))))
+
+(defun day10 (input &key (part 1))
+  (destructuring-bind (start-pos map) (get-map input)
+    (destructuring-bind (steps loop-squares)
+	(iter     
+	  (for (pos from steps root)
+	       in-bfs-from start-pos
+	       neighbours (lambda (n) (mapcar #'first (pipe-neighbours n map)))
+	       test 'equal
+	       single t)
+	  (collect pos into loop)
+	  (maximizing steps)
+	  (finally (return (list steps loop))))
+      (let* ((marked (mark-loop loop-squares))
+	     (outside-mark (walk-loop start-pos marked map))
+	     (count-marks (count-table marked)))
+	(if (= part 1)
+	    steps
+	    (gethash (if (eq outside-mark :a) :b :a) count-marks))))))
+
+
