1f493e4c76abcc9d979500638832039d2b483bf2
Blake Watkins <blakewatkins@gmail.com>
2023-12-17 19:36:42 +1300

    Day 17 part 1

---
+++ day17.lisp
@@ -0,0 +1,38 @@
+(in-package :aoc-2023)
+
+(defun parse-file ()
+  (parse-lines (one-or-more (parse-digit))))
+
+(defparameter *directions* '((-1 0) (0 1) (1 0) (0 -1) ))
+
+(defun turn (dir c-cw)
+  (elt *directions*
+       (mod (+ (position dir *directions* :test 'equal) (if (eq c-cw :cw) 1 -1))
+            (length *directions*))))
+
+(defun neighbours (pos dir straight map)
+  (let ((dirs `((,(turn dir :cw) 1) (,(turn dir :ccw) 1))))
+    (when (< straight 3) (push (list dir (1+ straight)) dirs))
+    (remove-if-not
+     (lambda (pos) (gethash pos map))
+     (mapcar (lambda (info) `((,(point+ pos (first info))
+                               ,(first info)
+                               ,(second info))
+                              ,(gethash (point+ pos (first info)) map)))
+             dirs)
+     :key #'caar)))
+
+
+(defun day17 (input)
+  (let* ((parsed (run-parser (parse-file) input))
+         (map (hash-table-from-list-list parsed)))
+    (iter
+      (with end = (hash-table-dimensions map))
+      (with parents = (make-hash-table :test 'equal))
+      (for (vertex parent distance) in-dijkstra-from '((0 0) (0 1) 0)
+           :neighbours (lambda (info)
+                         (neighbours (first info) (second info) (third info) map)))
+      (unless (gethash (first vertex) parents)
+        (setf (gethash (first vertex) parents) (first parent)))
+      (until (equal end (first vertex)))
+      (finally (return distance)))))
